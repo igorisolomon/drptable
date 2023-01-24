b5d0fdd6b074fa4184f1b0fcc52cbcdc196d4a82
igori <igorisolomon@gmail.com>
2023-01-24 13:32:20 +0000

    - Test for header

--- src/components/__tests__/Header.test.tsx
+++ src/components/__tests__/Header.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Header from '../Header';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Header />);
+  const linkElement = screen.getByText(/User List/i);
   expect(linkElement).toBeInTheDocument();
 });
